81a97c2b66b1a45f5ccb57a8ee10daaf11303d47
Arielpods <ariela.agba@edu.esiee-it.fr>
2025-10-09 11:56:35 +0200

    aesthetic

--- app/src/main/java/com/example/flashcard/ScoreActivity.java
+++ app/src/main/java/com/example/flashcard/ScoreActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -36,25 +37,44 @@ public class ScoreActivity extends AppCompatActivity {
         String difficulty = intent.getStringExtra("difficulty");
         int goodAnswers = intent.getIntExtra("goodAnswers", 0);
 
-        int totalQuestions = 0;
+        int totalQuestions;
         if (difficulty != null) {
             totalQuestions = Question.getQuestions(difficulty).size();
+        } else {
+            totalQuestions = 0;
         }
-        int percentageInt = 0;
+        int percentageInt;
         if (totalQuestions > 0) {
             percentageInt = Math.round(((float) goodAnswers / totalQuestions) * 100f);
+        } else {
+            percentageInt = 0;
         }
 
         DifficultyText.setText(difficulty);
         TextScore.setText( goodAnswers + "/" + totalQuestions);
         PercentageText.setText(percentageInt + "%");
 
-        Button homeButton = findViewById(R.id.homeButton);
+        ImageButton homeButton = findViewById(R.id.homeButton);
         homeButton.setOnClickListener(view -> {
             Intent HomeIntent = new Intent(this,HomeActivity.class);
             startActivity(HomeIntent);
         });
 
+
+        ImageButton shareButton = findViewById(R.id.shareButton);
+        shareButton.setOnClickListener(view -> {
+
+            String shareText = "J'ai eu " + goodAnswers + "/" + totalQuestions +
+                    " avec un score de :" + percentageInt + "%. " + "en "+ difficulty ;
+
+            // action share
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+
+            //  share menu
+            startActivity(Intent.createChooser(shareIntent, "Partager via"));
+        });
 
     }
 }
