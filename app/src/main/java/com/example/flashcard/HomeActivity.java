cb6db3035abb9038e03315f92a22501dae7e08e4
NidouMH4U <nidalfarrag@gmail.com>
2025-10-10 14:29:40 +0200

    finalver

--- app/src/main/java/com/example/flashcard/HomeActivity.java
+++ app/src/main/java/com/example/flashcard/HomeActivity.java
@@ -48,6 +48,10 @@ public class HomeActivity extends AppCompatActivity {
         setupHoverScale(aboutButton, 1.2f);
         setupHoverScale(questionButton, 1.2f);
 
+        // Question List
+        questionButton.setOnClickListener(v ->
+                startActivity(new Intent(HomeActivity.this, QuestionListActivity.class)));
+
         // AboutActivity
         aboutButton.setOnClickListener(v ->
                 startActivity(new Intent(HomeActivity.this, AboutActivity.class)));
